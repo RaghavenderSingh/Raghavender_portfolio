283c94f97a0ca77e5b2f39360292d24420708148
RaghavenderSingh <singhranu063@gmail.com>
2022-12-18 12:33:33 +0530

    Deploying

--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,29 @@
+
+import React, { useRef, useState } from 'react'
+
+
+
+
+
 const AboutPage = () => {
   return (
     <>
-      <h1>About Page</h1>
+      <h3>A Little Bit About Me</h3>
+      <h4>
+Building Products and tools with deep focus on problem at hand and 
+satisfying customer requirement with established and tested 
+procedure to attain optimised end result. Working in rapid 
+changing environment and maintaining the agility of systems.
+</h4>
+
+
     </>
   );
 };
 
 export async function getStaticProps() {
   return {
-    props: { title: "About" },
+    props: { title: 'About' },
   };
 }
 
